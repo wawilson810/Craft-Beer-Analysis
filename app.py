97c67e5d2bef2cc7601f42a35312cfa015ff5c93
damast100 <dmast5013@gmail.com>
2022-07-25 21:15:20 -0400

    uploading app file

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
+from flask import Flask, render_template
 from web_scrape import webscrape
 
-webscrape()
+app = Flask(__name__)
 
+
+@app.route("/")
+def index():
+	webscrape()
+	return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
